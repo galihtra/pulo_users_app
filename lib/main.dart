5f1914b98e1aae214a35125dce4ab734a57ea222
Galih Tri Risky Andiko <galihtririskyandiko@gmail.com>
2023-12-06 12:52:48 +0700

    add google maps & current location user

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:users_app/authentication/login_screen.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  
+  await Permission.locationWhenInUse.isDenied.then((valueOfPermission)
+  {
+    if(valueOfPermission)
+    {
+      Permission.locationWhenInUse.request();
+    }
+  });
 
   runApp(const MyApp());
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:users_app/global/global_var.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,18 +13,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final Completer<GoogleMapController> googleMapCompleterController =
+      Completer<GoogleMapController>();
+  GoogleMapController? controllerGoogleMap;
+  Position? currentPostionOfUser;
+
+  getCurrentLiveLocationOfUser() async {
+    Position positionOfUser = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.bestForNavigation);
+    currentPostionOfUser = positionOfUser;
+
+    LatLng positionOfUserInLatLng =
+        LatLng(currentPostionOfUser!.latitude, currentPostionOfUser!.longitude);
+
+    CameraPosition cameraPosition =
+        CameraPosition(target: positionOfUserInLatLng, zoom: 15);
+
+    controllerGoogleMap!
+        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Home Page",
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.white,
-          ),
-        ),
-      ),
-    );
+    return Scaffold(
+        body: Stack(
+      children: [
+        GoogleMap(
+          mapType: MapType.normal,
+          myLocationEnabled: true,
+          initialCameraPosition: googlePlexInitialPosition,
+          onMapCreated: (GoogleMapController mapController) {
+            controllerGoogleMap = mapController;
+            googleMapCompleterController.complete(controllerGoogleMap);
+            getCurrentLiveLocationOfUser();
+          },
+        )
+      ],
+    ));
   }
 }
