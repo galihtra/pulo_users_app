7569f8320bab46594f93d00953f7eb5ac947974c
Galih Tri Risky Andiko <galihtririskyandiko@gmail.com>
2024-01-03 22:32:37 +0700

    done for user

---
+++ lib/global/trip_rider_var.dart
@@ -0,0 +1,7 @@
+String nameRider = '';
+String photoRider = '';
+String phoneNumberRider = '';
+int requestTimeoutRider = 20;
+String status = '';
+String carDetailsRider = '';
+String tripStatusDisplay = 'Driver is Arriving';
